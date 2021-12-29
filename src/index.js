90fc7dce8d199bfe3d8baa53ef74efbb917275e5
mohahdy <mohahdyc@hotmail.com>
2021-12-30 00:21:34 +0200

    added loading bar

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+import LoadingBar from 'react-redux-loading-bar'
 const store = createStore(reducer,middleware);
 class ProviderApp extends React.Component{
   render(){
     return(
       <Provider store={store}>
+         <LoadingBar/>
       <App/>
       </Provider>
     )
