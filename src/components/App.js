e6328bff897952d8234c98cce0cbd8ac0eec4302
mohahdy <mohahdyc@hotmail.com>
2021-12-30 18:54:44 +0200

    implementing new tweet component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
 import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
 import LoadingBar from 'react-redux-loading-bar'
 class App extends Component {
   componentDidMount(){
@@ -13,7 +14,7 @@ class App extends Component {
       <div>
         <LoadingBar/>
       {this.props.authedUser===null?null:
-        <Dashboard/>
+        <NewTweet/>
       }
       </div>
 
--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -3,8 +3,11 @@ import React, {Component} from 'react'
 class NewTweet extends Component {
   render() {
     return(
-      <div>
-        New Tweet
+      <div className="center">
+      <h3>New Tweet</h3>
+        <form>
+        <input type="submit"/>
+        </form>
       </div>
     )}
 }
