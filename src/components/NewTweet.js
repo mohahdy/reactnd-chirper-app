3a2ee4b45620506786cfbf45521bdad860c8ff90
mohahdy <mohahdyc@hotmail.com>
2021-12-30 22:08:33 +0200

    Updated UI of the New Tweet component.

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react'
 
 class NewTweet extends Component {
+  state = {
+    text:''
+  }
+  handleChange = (e) => {
+    const text = e.target.value;
+    this.setState(()=>({text}))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted text: ', this.state.text)
+    this.setState(()=>({text:''}))
+
+  }
+
   render() {
+    const {text} = this.state
     return(
       <div className="center">
       <h3>New Tweet</h3>
-        <form>
-        <input type="submit"/>
+        <form className = "new-tweet">
+        <textarea 
+        placeholder = "What is happening?"
+        value = {text}
+        onChange = {this.handleChange}
+        className = "textarea"
+        maxLength={280}/>
         </form>
+        <button type="submit" className="btn" type="submit" onClick={this.handleSubmit} disabled={this.state.text===''}>
+          Submit
+        </button>
       </div>
     )}
 }
