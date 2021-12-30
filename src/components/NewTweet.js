f9d28bfe1e8ed296fe9ed5e220a35e540ce9e4be
mohahdy <mohahdyc@hotmail.com>
2021-12-30 18:11:01 +0200

    Added NewTweet Component

---
+++ src/components/NewTweet.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class NewTweet extends Component {
+  render() {
+    return(
+      <div>
+        New Tweet
+      </div>
+    )}
+}
+
+export default  NewTweet
